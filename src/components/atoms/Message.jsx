70c446e939a14dc60f2cfd033a54c9157a30be9f
Pankaj Bora <borapankaj901@gmail.com>
2023-02-01 09:19:39 +0530

    fix: message scroll

--- src/components/atoms/Message.jsx
+++ src/components/atoms/Message.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useChat } from "../../context/ChatContext";
 import { useAuth } from "../../context/UserContext";
 import localClasses from "./message.module.scss";
@@ -6,9 +6,15 @@ const Message = (props) => {
   const { message } = props;
   const { data } = useChat();
   const { user } = useAuth();
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "instant" });
+  }, [message]);
+
   const name = user.uid == message.senderId ? null : data.user.name;
   return (
-    <div className={name ? localClasses.other : localClasses.self}>
+    <div className={name ? localClasses.other : localClasses.self} ref={ref}>
       <p className={name ? localClasses.otherName : localClasses.selfName}>
         {name ? name : "You"}
       </p>
