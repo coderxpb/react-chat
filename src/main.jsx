f09f811a9fe54637b250c5fb6cdf7f82669eeac2
Pankaj Bora <borapankaj901@gmail.com>
2023-02-01 11:47:11 +0530

    fix: repo routing

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
       <ChatContextProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/react-chat/">
           <App />
         </BrowserRouter>
       </ChatContextProvider>
